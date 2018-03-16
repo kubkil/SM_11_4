404c7700c775cc19ab867a02f3db19f64daed701
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-16 21:56:28 +0100

    add constructor function and prototypes

--- js/script-dist.js
+++ js/script-dist.js
@@ -1,2 +1,34 @@
 'use strict';
+
+function Phone(brand, price, color) {
+  //constructor function, named with capital Letter, executed with 'new' operator - capital letter indicates that is should be run with 'new'
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+}
+
+Phone.prototype.printInfo = function () {
+  console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' PLN.');
+};
+
+var iPhone6S = new Phone('Apple', 2250, 'silver');
+var SamsungX = new Phone('Samsung', 1500, 'black');
+var HuaweiY = new Phone('Huawei', 3000, 'blue');
+
+iPhone6S.printInfo();
+SamsungX.printInfo();
+HuaweiY.printInfo();
+
+Phone.prototype.winterSale = function () {
+  var date = void 0;
+  for (date = 0; date <= 31; date++) {
+    if (date >= 20 && date < 28) {
+      console.log('Winter sale price for ' + this.brand + ', color ' + this.color + ' is ' + this.price * 0.75 + ' PLN.');
+    }
+  }
+};
+
+iPhone6S.winterSale();
+SamsungX.winterSale();
+HuaweiY.winterSale();
 //# sourceMappingURL=script-dist.js.map
